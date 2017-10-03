c5d55077971f6d41f81f2f48ec7a2ff2fe29c106
ultraub <32494040+ultraub@users.noreply.github.com>
2017-10-03 19:01:58 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,6 +7,9 @@ using namespace std;
 
 int main (int argc, char * argv []) {
     int game = 0, location =1;
+    Character player;
+    player.totalHealth = 10;
+    player.printname(); 
     // Loop while game has not ended
     while (game!=1) {
         switch (location) {
