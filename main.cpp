8ed4b731fe27bd6a2623463cadcff4172790d0e1
ultraub <32494040+ultraub@users.noreply.github.com>
2017-10-03 14:47:00 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,20 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 #include "functions.hpp"
 
 using namespace std; 
 
 int main (int argc, char * argv []) {
   
-    cout << "Hello World!" << endl;
-    cout << "I'm gay!" << endl; 
+    string inputStr;
+	
+    cout << "You wake up in a house" << endl; 
+	cout << "What is your name?" << endl;
+	// Get first options to move
+	// Make object chest, inside is knife, other objects??
+	getline (cin, inputStr); 
+	cout << "Hello " << inputStr << endl;
 	int y = 5;
 	int *z = &y;
 	firstFunction(z);
