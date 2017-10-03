03efe30c9e2ff653c45cc2bf8a93d0edc5eea95b
ultraub <32494040+ultraub@users.noreply.github.com>
2017-10-03 14:29:54 -0400

    Update functions.hpp

--- functions.hpp
+++ functions.hpp
@@ -1,7 +1,15 @@
+
 //AIDS
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
 void firstFunction (int*);
 
 void firstFunction (int* x) {
-	x = 10;
-    cout << "The new value is " << x << endl;
+        *x = 10;
+    	cout << "The new value is " << *x << endl;
 }
+
