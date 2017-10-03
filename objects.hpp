295f6775d1f42bfba162bb34e88731f3dd76904b
ultraub <32494040+ultraub@users.noreply.github.com>
2017-10-03 18:40:51 -0400

    Update objects.hpp

--- objects.hpp
+++ objects.hpp
@@ -1,1 +1,24 @@
 // objects like characters
+
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+class Character
+{
+    // Access specifier
+    public:
+
+    // 
+    string charname = "faggot";
+    string primary;
+    string secondary;
+	int totalHealth = 10;
+	int 
+    // Member Functions()
+    void printname()
+    {
+       cout << "Character name is: " << charname;
+    }
+};
