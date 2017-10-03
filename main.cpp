01d78653a7b70b54e9bc0e78af6894c889ed217a
ultraub <32494040+ultraub@users.noreply.github.com>
2017-10-03 17:24:56 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,17 +6,5 @@
 using namespace std; 
 
 int main (int argc, char * argv []) {
-  
-    string inputStr;
-	
-    cout << "You wake up in a house" << endl; 
-	cout << "What is your name?" << endl;
-	// Get first options to move
-	// Make object chest, inside is knife, other objects??
-	getline (cin, inputStr); 
-	cout << "Hello " << inputStr << endl;
-	int y = 5;
-	int *z = &y;
-	firstFunction(z);
-	cout << *z << endl;
-}
+  	house ();
+  }
