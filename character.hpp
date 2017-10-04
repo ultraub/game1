b5fd978e3eaf5ea5467af6be60b0973977ad0e9d
ultraub <32494040+ultraub@users.noreply.github.com>
2017-10-04 10:30:06 -0400

    Create character.hpp

---
+++ character.hpp
@@ -0,0 +1,17 @@
+#include "objects.hpp"
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+#ifndef CHARACTER_HPP
+#define CHARACTER_HPP
+
+Character player;
+player.charname = "Fagot";
+player.totalHealth = 10;
+player.location = 1;
+
+
+#endif
+
