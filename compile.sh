e0fd700a1656fe1aeb5062c58c47314d4eb5d545
ultraub <32494040+ultraub@users.noreply.github.com>
2017-10-04 18:51:43 -0400

    Delete compile.sh

--- compile.sh
+++
@@ -1,5 +0,0 @@
-#Use this shell to compile 
-#Make sure to add any files to the makefile that should be added
-
-g++ house.cpp main.cpp funtions.hpp beach.cpp forest.cpp graveyard.cpp objects.hpp -o game -Wall
-exit 0
