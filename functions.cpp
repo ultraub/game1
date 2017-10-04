869abf851e077d0b4fbee4120dc5b5ec235e00f8
ultraub <32494040+ultraub@users.noreply.github.com>
2017-10-04 00:40:50 -0400

    Create functions.cpp

---
+++ functions.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+void readData (FILE * inFile) {
+  if (inFile.is_open()) {
+    // Read from file and put in object that understands the data
+    }
+}
+
+void saveData (FILE* inFile) {
+    // Writes to file with whatever is inside the object so it can be read later
+}
+   
+  
