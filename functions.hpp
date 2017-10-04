7df3f5143dcc0b50c5ebcc3f9d53d470e8dc10a8
ultraub <32494040+ultraub@users.noreply.github.com>
2017-10-04 00:34:45 -0400

    Update functions.hpp

--- functions.hpp
+++ functions.hpp
@@ -7,7 +7,10 @@
 using namespace std;
 
 void firstFunction (int*);
-
+//Reads file that may exist for saving data 
+void readData(FILE *);
+//Writes to file to save game data
+void saveData(FILE *);
 void firstFunction (int* x) {
 
 }
