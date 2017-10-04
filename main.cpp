020fe8830279cd8dbffe449ff5a6a1121e15c00d
ultraub <32494040+ultraub@users.noreply.github.com>
2017-10-04 09:50:03 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -26,7 +26,7 @@ int main (int argc, char * argv []) {
                 house();
                 break;
             case 2: 
-                //forest();
+                forest();
                 break;
             default:
                 game =1;
