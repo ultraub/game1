16645f22dfd5fd3b9e086f663ff784ae0c92b020
Papalump <32494048+papalump@users.noreply.github.com>
2017-10-04 23:17:46 -0400

    Update forest.cpp

--- forest.cpp
+++ forest.cpp
@@ -27,7 +27,28 @@ int forest (Character& player) {
 	
 	cout << "Despite that sudden, bloody encounter, your spirits begin to lift as you survey the forest around you." << endl;
 	cout << "What would you like to do?" << endl;
+	cout << "1. Go to the left." << endl << "2. Go to the right" << endl << "3. Look up" << endl;
+	cin >> choice;
+	int flag = 0;
+	int choice = 0;
 	
+	/*
+	while (flag == 0) {
+		cin >> choice;
+		if (choice == 1) {
+			cout << "You begin walking " << endl;
+			flag = 1;
+		}
+		else if (choice == 2) {
+			cout << "" << endl;
+			flag = 1;
+		}
+		else {
+			cout << "Please enter a valid command." << endl;
+			cin >> choice;
+		}
+	}
+	*/
     return 1; 
     
 }
