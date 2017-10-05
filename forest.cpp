137bb6c3a5143a6de057ef84f5f3ed1a6712dc41
Papalump <32494048+papalump@users.noreply.github.com>
2017-10-04 20:49:28 -0400

    Update forest.cpp

--- forest.cpp
+++ forest.cpp
@@ -7,7 +7,9 @@
 using namespace std;
 int forest () {
     //Inset code here
-    cout << "You are now in the forest" << endl;
+    cout << "You step outside into the forest. Before you have a chance to look around, a wolf jumps from the shadows, biting at your throat." << endl;
+    
+    
     return 1; 
     
 }
