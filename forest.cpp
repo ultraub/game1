d9b7fad566a591e99009185beb95b4b86609599c
Papalump <32494048+papalump@users.noreply.github.com>
2017-10-06 22:04:59 -0400

    Update forest.cpp

--- forest.cpp
+++ forest.cpp
@@ -36,11 +36,12 @@ int forest (Character& player) {
 	
 	cout << "Despite that sudden, bloody encounter, your spirits begin to lift as you survey the forest around you." << endl;
 	cout << "What would you like to do?" << endl;
-	cout << "1. Go to the left." << endl << "2. Go to the right" << endl << "3. Look up" << endl;
+	cout << "1. Go to the left." << endl << "2. Go to the right" << endl << "3. Look around" << endl;
 	int flag = 0;
 	int choice = 0;
 	
 	while (flag == 0) {
+		int choice = 0;
 		cin >> choice;
 		if (choice == 1) {
 			cout << "You walk along the edge of the forest for about a mile until you stumble across a quaint beach. As far as you can tell, no one's been here in ages" << endl;
@@ -51,6 +52,42 @@ int forest (Character& player) {
 			cout << "You walk along the edge of the forest for about a mile until you notice a small, decrepit looking graveyard to your left." << endl;
 			flag = 1;
 			return 4;
+		}
+		else if (choice == 3) {
+			cout << "You gaze at the forest around you. The tall evergreens shift slightly in the wind. Above you, the sun begins its descent in the sky and you guess you have at least an hour until it is set. Despite how ancient the house looks, it holds a comforting air about it. The air is cool and eerily silent." << endl;
+			cout << "Would you like to walk around the house?" << endl;
+			cout << "1. Yes" << endl << "2. No" << endl;
+			while (flag == 0) {
+				cin >> choice;
+				if (choice == 1) {
+					cout << "You make your way to the rear of the house where you find a small garden. Nothing seems be growing in it." << endl;
+					cout << "Would you like to dig in the garden?" << endl;
+					cout << "1. Yes" << endl << "2. No" << endl;
+					while (flag == 0) {
+						cin >> choice;
+						if (choice == 1) {
+							cout >> "You get down on your knees and sift through the dry dirt." << endl;
+							cout >> "You found a leather helmet." << endl;
+							//Set leatherHelmet = 1
+							flag++;
+						else if (choice == 2) {
+							cout >> "You return to the front of the house." << endl;
+							flag++;
+						}
+						else {
+							cout << "Please enter a valid command." << endl;
+							cin >> choice;
+						}
+					}
+					else if (choice == 2) {
+						flag++;
+					}
+					else {
+						cout << "Please enter a valid command." << endl;
+						cin >> choice;
+					}
+				}
+			}
 		}
 		else {
 			cout << "Please enter a valid command." << endl;
