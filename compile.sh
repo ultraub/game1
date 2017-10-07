9bcd18b2efb3fd550e40ef244433fc877c7879fe
ultraub <32494040+ultraub@users.noreply.github.com>
2017-10-07 15:24:55 -0400

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 #Use this shell to compile 
 #Make sure to add any files to the makefile that should be added
 
-g++ house.cpp main.cpp funtions.hpp beach.cpp forest.cpp graveyard.cpp objects.hpp -o game -Wall
+g++ house.cpp main.cpp funtions.hpp beach.cpp forest.cpp graveyard.cpp objects.hpp beach.cpp -Wall -std=c++11
 exit 0
